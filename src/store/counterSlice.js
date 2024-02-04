03f7e371204f137ffa7a1a9aa7eda8e6da0019d0
jaymhorsh <jaji.moshood97@yahoo.com>
2024-02-04 16:26:14 +0100

    dispatch with payload

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -1,20 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,configureStore } from "@reduxjs/toolkit";
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { value: 0 },
-    reducers: {
-      increment: (state) => {
-        state.value += 1;
-      },
-      decrement: (state) => {
-        state.value -= 1;
-      },
-      clearCount:(state)=>{
-        state.value = 0
-      }
+  initialState:{ value: 0},
+  reducers: {
+    increment: (state) => {
+      state.value ++;
+    },
+    decrement: (state) => {
+      // state.value += 1
+      state.value --;
+    },
+    //Adding payload
+    increasrByFive:(state, action)=>{
+      state.value= state.value +  action.payload.amount
+    },
+    clearCount: (state) => {
+      state.value = 0;
+    },
   },
 });
-export const {  increment,decrement,clearCount } = counterSlice.actions;
-export default counterSlice.reducer;
+export const { increment, decrement,increasrByFive, clearCount } = counterSlice.actions;
+// export const counterAction =  counterSlice.actions
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
 
-
+export default store;
